25b20ad4e131cd9b239c4b00056cb71eac0e80c0
Ujjwal Patel <ujjwalp476@gmail.com>
2025-06-26 18:41:13 +0530

    ADD:Donate form be api

--- src/page/donate/DonateFoodForm.jsx
+++ src/page/donate/DonateFoodForm.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import axios from "axios";
 
 const DonateFoodForm = () => {
   const [formData, setFormData] = useState({
@@ -45,15 +46,25 @@ const DonateFoodForm = () => {
   };
 
   const donateFood = async (data) => {
+    console.log(data);
     try {
       const response = await fetch(
-        "https://notification-backend-a6eab-default-rtdb.firebaseio.com/donations.json",
+        import.meta.env.VITE_APP_BASE_URL + "admin/request",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(data),
+          body: {
+            donorName: data?.name,
+            donarEmail: data?.email,
+            donarMobile: data?.contact,
+            pickUpAddress: data?.address,
+            foodCategory: data?.category,
+            foodQuantity: data?.quantity,
+            preparationDateTime: data?.prepTime,
+            note: data?.note,
+          },
         }
       );
 
